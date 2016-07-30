dd14e1cf73f902b792d01053318b31366a471f27
sdnr1 <sidnayyar@outlook.com>
2016-07-30 13:15:41 +0530

    algorithm.py bug fix

--- webapp/picker/utils/algorithm.py
+++ webapp/picker/utils/algorithm.py
@@ -1,28 +1,24 @@
 import numpy as np
 import pandas as pd
-import os
 
 def init():
-    module_dir = os.path.dirname(__file__)
-    scraped = pd.read_csv(os.path.join(module_dir,'hero_data.csv'))
-    scraped = scraped.as_matrix()
-    # scraped = scraped[1:][:]
+    scraped = pd.read_csv('hero_data.csv', header=None).as_matrix()
     numHeroes = scraped.shape[0]
     return (numHeroes, scraped)
 
 def add(newRate, currRate):
     return newRate + currRate
 
-def picker(numHeroes, enemyTeam, pickedHeroes, scraped):
+def picker(numHeroes, enemyTeam, pickedHeroes, scraped, winRate):
     for enemyHero in enemyTeam:
         for i in range(numHeroes):
-            winRate[i] = (add(scraped[i][enemyHero], winRate[i][1]), i)
-    winrate.sort()
+            winRate[i] = (add(scraped[enemyHero][i], winRate[i][0]), i)
+    winRate.sort()
     bestHeroes = []
-    for i in range(110):
-        if winrate[-i][1] not in pickedHeroes:
-            bestHeroes.add(winRate[-i][1])
-        if bestHeroes.length == 5:
+    for i in range(numHeroes):
+        if winRate[-i-1][1] not in pickedHeroes:
+            bestHeroes.append(winRate[-i-1][1])
+        if len(bestHeroes) == 5:
             break
     return bestHeroes
 
@@ -33,5 +29,10 @@ def main(enemyTeam, ourTeam):
         temp=(0, i)
         winRate.insert(i,temp)
     pickedHeroes = enemyTeam + ourTeam
-    bestHeroes = picker(numHeroes, enemyTeam, pickedHeroes, scraped)
+    bestHeroes = picker(numHeroes, enemyTeam, pickedHeroes, scraped, winRate)
     return bestHeroes
+
+enemyTeam = []
+ourTeam = []
+bestHeroes = main(enemyTeam, ourTeam)
+print(bestHeroes)
