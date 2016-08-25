3adc8c81a1178cde103b35d6ed4735fe604306e0
guptachetan1997 <gupta.chetan1997@gmail.com>
2016-08-25 15:03:53 +0530

    minor fixes in api

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -105,6 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
     )
 }
 
@@ -126,3 +127,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn")
